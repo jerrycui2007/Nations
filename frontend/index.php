51af14c86a1b63c4f493e0d682727c5a93ca6d02
Jerry Cui <125308969+jerrycui2007@users.noreply.github.com>
2024-10-22 10:40:40 -0400

    Fixed another bug

--- frontend/index.php
+++ frontend/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nations - Welcome</title>
-    <link rel="stylesheet" type="text/css" href="../design/style.css">
+    <link rel="stylesheet" type="text/css" href="design/style.css">
 </head>
 <body>
 
